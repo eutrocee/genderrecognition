891b024bc4fb4e0910f68361ed476cb013983c06
Jae Min An <veryneuron@gmail.com>
2021-11-15 18:00:24 +0900

    librosa library test

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-# This is a sample Python script.
+import numpy as np
+import matplotlib.pyplot as plt
+import librosa, librosa.display
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+FIG_SIZE=(15, 10)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+file = 'voice1.wav'
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+data, sampling_rate = librosa.load(file, sr=44100)
+
+print(data, data.shape)
+
+#plt.figure(figsize=FIG_SIZE)
+#librosa.display.waveplot(data, sampling_rate, alpha=0.5)
+#plt.xlabel("Time (s)")
+#plt.ylabel("Amplitude")
+#plt.title("Waveform")
