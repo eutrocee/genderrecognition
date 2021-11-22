30fc78172ff97f97847bdfb7514f65cae929e501
veryneuron <veryneuron1@gmail.com>
2021-11-23 02:58:43 +0900

    dtw matching and plotting

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt
 import librosa, librosa.display
 from scipy.signal.windows import hamming
 import math
 
 frame = 256
-target_sample_rate = 8000
+sampling_rate = 8000
 overlapping_rate = 0.5
 order = 12
 
@@ -49,14 +50,67 @@ female1_co = extract_features(data_female_1)
 female2_co = extract_features(data_female_2)
 
 #change number to change order of coefficient
-plt.plot(male1_co[:, 2], 'b', label="male1")
-plt.plot(male2_co[:, 2], 'c', label="male2")
-plt.plot(female1_co[:, 2], 'r', label="female1")
-plt.plot(female2_co[:, 2], 'm', label="female2")
-plt.legend()
-plt.xlabel("Order of frames")
-plt.ylabel("Coefficient value")
-plt.title("lpc coefficient")
+
+#plt.plot(male1_co[:, 1], 'b', label="male1")
+#plt.plot(male2_co[:, 1], 'c', label="male2")
+#plt.plot(female1_co[:, 1], 'r', label="female1")
+#plt.plot(female2_co[:, 1], 'm', label="female2")
+#plt.legend()
+#plt.xlabel("Order of frames")
+#plt.ylabel("Coefficient value")
+#plt.title("lpc coefficient")
 #plt.xlim(0, 60)
-plt.xlim(0, 300)
+#plt.xlim(0, 300)
+#plt.show()
+
+#align feature sequence with dtw
+
+male_dtw, male_wp = librosa.sequence.dtw(male1_co[:, 1].T, male2_co[:, 1].T)
+female_dtw, female_wp = librosa.sequence.dtw(female1_co[:, 1].T, female2_co[:, 1].T)
+
+male_wp_size = np.asarray(male_wp) * frame / sampling_rate
+
+fig = plt.figure(figsize = (10, 10))
+ax = fig.add_subplot(111)
+librosa.display.specshow(male_dtw, x_axis='time', y_axis='time',
+                         cmap = 'gray_r', hop_length = frame)
+imax = ax.imshow(male_dtw, cmap = plt.get_cmap('gray_r'),
+                 origin='lower', interpolation = 'nearest', aspect = 'auto')
+ax.plot(male_wp_size[:, 1], male_wp_size[:, 0], marker = 'o', color = 'r')
+plt.title('Warping Path')
+plt.colorbar()
 plt.show()
+
+fig_male = plt.figure(figsize=(16, 8))
+
+plt.subplot(2, 1, 1)
+librosa.display.waveplot(data_male_1, sr = sampling_rate)
+plt.title('male1')
+ax1 = plt.gca()
+
+plt.subplot(2, 1, 2)
+librosa.display.waveplot(data_male_2, sr = sampling_rate)
+plt.title('male2')
+ax2 = plt.gca()
+
+plt.tight_layout()
+
+trans_figure_male = fig_male.transFigure.inverted()
+lines = []
+arrows = 30
+points_idx_male = np.int16(np.round(np.linspace(0, male_wp.shape[0] - 1, arrows)))
+
+for tp1, tp2 in male_wp[points_idx_male] * frame / sampling_rate:
+    coord1 = trans_figure_male.transform(ax1.transData.transform([tp1, 0]))
+    coord2 = trans_figure_male.transform(ax2.transData.transform([tp2, 0]))
+
+    line = matplotlib.lines.Line2D((coord1[0], coord2[0]),
+                                   (coord1[0], coord2[0]),
+                                   transform = fig_male.transFigure,
+                                   color = 'r')
+
+    lines.append(line)
+
+fig_male.lines = lines
+plt.tight_layout()
+
