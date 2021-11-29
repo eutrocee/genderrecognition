15681bff615459c62d6cc030e726b1cfdad9ccb5
veryneuron <veryneuron1@gmail.com>
2021-11-30 00:50:31 +0900

    load reference data from ref folder. identify speaker gender by compare dtw distance with reference speaker. closest speaker's gender is input speaker's gender. if distance is over 120, then print cannot identify speaker

--- main.py
+++ main.py
@@ -1,55 +1,62 @@
-import numpy as np
 import librosa
 import os
+import sys
 from lpc import extract_features
-from plotting import plot_lpc_coefficient_compare4, plot_dtw_path
 from dtw import calculate_dtw_distance
 
 frame = 256
 sampling_rate = 8000
 overlapping_rate = 0.5
 order = 12
+threshold = 120
+
+# load & setting reference data
 path_male = './audiodata/refdata/male/'
-path_female = './audiodata/refdata/female/'
-ref_frame = 70
-
-# make reference data
 file_list_male = os.listdir(path_male)
-index_file = 0
-data_co = np.array(0)
+path_female = './audiodata/refdata/female/'
+file_list_female = os.listdir(path_female)
 
-# ref_data_1 = librosa.load(path_male + file_list_male[0], sampling_rate)
-# ref_data_co_1 = extract_features(ref_data_1, frame, overlapping_rate, order)
-# ref_data_2 = librosa.load(path_male + file_list_male[1], sampling_rate)
-# ref_data_co_2 = extract_features(ref_data_2, frame, overlapping_rate, order)
-# ref_data_3 = librosa.load(path_male + file_list_male[2], sampling_rate)
-# ref_data_co_3 = extract_features(ref_data_3, frame, overlapping_rate, order)
-#
-# max_len = max([ref_data_co_1.shape[0], ref_data_co_2.shape[0], ref_data_co_3.shape[0]])
+ref_data_male_1, sr_ref = librosa.load(path_male + file_list_male[0], sampling_rate)
+ref_data_male_2, sr_ref = librosa.load(path_male + file_list_male[1], sampling_rate)
+ref_data_male_3, sr_ref = librosa.load(path_male + file_list_male[2], sampling_rate)
+ref_data_female_1, sr_ref = librosa.load(path_female + file_list_female[0], sampling_rate)
+ref_data_female_2, sr_ref = librosa.load(path_female + file_list_female[1], sampling_rate)
+ref_data_female_3, sr_ref = librosa.load(path_female + file_list_female[2], sampling_rate)
 
+ref_data_co_male_1 = extract_features(ref_data_male_1, frame, overlapping_rate, order)
+ref_data_co_male_2 = extract_features(ref_data_male_2, frame, overlapping_rate, order)
+ref_data_co_male_3 = extract_features(ref_data_male_3, frame, overlapping_rate, order)
+ref_data_co_female_1 = extract_features(ref_data_female_1, frame, overlapping_rate, order)
+ref_data_co_female_2 = extract_features(ref_data_female_2, frame, overlapping_rate, order)
+ref_data_co_female_3 = extract_features(ref_data_female_3, frame, overlapping_rate, order)
 
 # load data + data normalization
-data_male_1, sr1 = librosa.load('audiodata/male_3.wav', sampling_rate)
-data_male_2, sr2 = librosa.load('audiodata/male_4.wav', sampling_rate)
-data_female_1, sr3 = librosa.load('audiodata/female_3.wav', sampling_rate)
-data_female_2, sr4 = librosa.load('audiodata/female_4.wav', sampling_rate)
+print('Input file:', sys.argv[1])
+input_data, sr = librosa.load('./audiodata/' + sys.argv[1], sampling_rate)
 
 # feature extracting
-male1_co = extract_features(data_male_1, frame, overlapping_rate, order)
-male2_co = extract_features(data_male_2, frame, overlapping_rate, order)
-female1_co = extract_features(data_female_1, frame, overlapping_rate, order)
-female2_co = extract_features(data_female_2, frame, overlapping_rate, order)
+input_data_co = extract_features(input_data, frame, overlapping_rate, order)
 
 # plotting
-plot_lpc_coefficient_compare4(female1_co, female2_co, male2_co, 1)
-# plot_dtw_path(male1_co, male2_co, 1)
+# plot_lpc_coefficient_compare4(input_data_co, ref_data_co_male_1, ref_data_co_male_2, ref_data_co_female_1, 1)
+# plot_dtw_path(input_data_co, ref_data_co_male_1, 1)
 
 # calculate dtw distance
-output = calculate_dtw_distance(male1_co, female1_co)
-output2 = calculate_dtw_distance(male2_co, female2_co)
-output3 = calculate_dtw_distance(female1_co, female2_co)
-output4 = calculate_dtw_distance(male1_co, male2_co)
-print('male1 - female1', output.sum())
-print('male2 - female2',output2.sum())
-print('female1 - female2',output3.sum())
-print('male1 - male2',output4.sum())
+min_distance_male = min(sum(calculate_dtw_distance(input_data_co, ref_data_co_male_1)),
+                        sum(calculate_dtw_distance(input_data_co, ref_data_co_male_2)),
+                        sum(calculate_dtw_distance(input_data_co, ref_data_co_male_3)))
+
+min_distance_female = min(sum(calculate_dtw_distance(input_data_co, ref_data_co_female_1)),
+                          sum(calculate_dtw_distance(input_data_co, ref_data_co_female_2)),
+                          sum(calculate_dtw_distance(input_data_co, ref_data_co_female_3)))
+
+# identify speaker
+if (min_distance_male < min_distance_female) and (min_distance_male < threshold):
+    print('Speaker is male')
+    print('Distance:', min_distance_male)
+elif min_distance_female < threshold:
+    print('Speaker is female')
+    print('Distance:', min_distance_female)
+else:
+    print('Cannot identify speaker!')
+    print('Distance:', min(min_distance_male, min_distance_female))
