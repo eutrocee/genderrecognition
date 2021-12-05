bb9bec4247f40fff7a9aba6026344c5d2dfbe28a
JaeMin An <veryneuron1@gmail.com>
2021-12-05 21:55:30 +0900

    change # of ref to 4

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ frame = 256
 sampling_rate = 8000
 overlapping_rate = 0.5
 order = 12
-threshold = 120
+threshold = 150
 
 # load & setting reference data
 path_male = './audiodata/refdata/male/'
@@ -19,16 +19,20 @@ file_list_female = os.listdir(path_female)
 ref_data_male_1, sr_ref = librosa.load(path_male + file_list_male[0], sampling_rate)
 ref_data_male_2, sr_ref = librosa.load(path_male + file_list_male[1], sampling_rate)
 ref_data_male_3, sr_ref = librosa.load(path_male + file_list_male[2], sampling_rate)
+ref_data_male_4, sr_ref = librosa.load(path_male + file_list_male[3], sampling_rate)
 ref_data_female_1, sr_ref = librosa.load(path_female + file_list_female[0], sampling_rate)
 ref_data_female_2, sr_ref = librosa.load(path_female + file_list_female[1], sampling_rate)
 ref_data_female_3, sr_ref = librosa.load(path_female + file_list_female[2], sampling_rate)
+ref_data_female_4, sr_ref = librosa.load(path_female + file_list_female[3], sampling_rate)
 
 ref_data_co_male_1 = extract_features(ref_data_male_1, frame, overlapping_rate, order)
 ref_data_co_male_2 = extract_features(ref_data_male_2, frame, overlapping_rate, order)
 ref_data_co_male_3 = extract_features(ref_data_male_3, frame, overlapping_rate, order)
+ref_data_co_male_4 = extract_features(ref_data_male_4, frame, overlapping_rate, order)
 ref_data_co_female_1 = extract_features(ref_data_female_1, frame, overlapping_rate, order)
 ref_data_co_female_2 = extract_features(ref_data_female_2, frame, overlapping_rate, order)
 ref_data_co_female_3 = extract_features(ref_data_female_3, frame, overlapping_rate, order)
+ref_data_co_female_4 = extract_features(ref_data_female_4, frame, overlapping_rate, order)
 
 # load data + data normalization
 print('Input file:', sys.argv[1])
@@ -44,11 +48,13 @@ input_data_co = extract_features(input_data, frame, overlapping_rate, order)
 # calculate dtw distance
 min_distance_male = min(sum(calculate_dtw_distance(input_data_co, ref_data_co_male_1)),
                         sum(calculate_dtw_distance(input_data_co, ref_data_co_male_2)),
-                        sum(calculate_dtw_distance(input_data_co, ref_data_co_male_3)))
+                        sum(calculate_dtw_distance(input_data_co, ref_data_co_male_3)),
+                        sum(calculate_dtw_distance(input_data_co, ref_data_co_male_4)))
 
 min_distance_female = min(sum(calculate_dtw_distance(input_data_co, ref_data_co_female_1)),
                           sum(calculate_dtw_distance(input_data_co, ref_data_co_female_2)),
-                          sum(calculate_dtw_distance(input_data_co, ref_data_co_female_3)))
+                          sum(calculate_dtw_distance(input_data_co, ref_data_co_female_3)),
+                          sum(calculate_dtw_distance(input_data_co, ref_data_co_female_4)))
 
 # identify speaker
 if (min_distance_male < min_distance_female) and (min_distance_male < threshold):
